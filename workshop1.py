7001976587ff7610affe320e64aa184ed24ec080
peek <nuttarin,ja@gmail.com>
2026-07-15 09:06:33 +0700

    edit workshop1 and 2

--- workshop1.py
+++ workshop1.py
@@ -6,5 +6,7 @@ team_members = int(input("จํานวนคนในทีม :"))
 print("ต้นทุน", quantity * cost)
 print("รายได้ทั้งหมด", sell_price * quantity)
 print("กําไร", (sell_price - cost) * quantity)
-print("บอสเอาไปกิน", ((sell_price - cost) * quantity) * 100/20)
+print("บอสเอาไปกิน", ((sell_price - cost) * quantity) * 20/100)
+print("ลูกน้องแบ่งกัน", (((sell_price - cost) * quantity) - ((sell_price - cost) * quantity) * 20/100)/team_members) 
+
 
--- workshop2.py
+++ workshop2.py
@@ -2,3 +2,13 @@ name = str(input("ใส่ชื่อของคุณ :"))
 age = int(input("ใส่อายุ :"))
 power = int(input("ระดับพละกําลัง :"))
 money = int(input("inputเงินติดกระเป๋า :"))
+
+if power >= 7:
+    print ("ผ่าน ได้รับตําแหน่งผู้รักษา")
+elif money >= 100000:
+    print ("ผ่าน ได้รับตําแหน่งกระเป๋าตังค์")
+elif age <= 24:
+    print ("ผ่าน คุณได้รับตําแหน่งดาวรุ่ง")
+else:
+    print ("ไปไหนก็ไป")
+     
